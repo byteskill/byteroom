058b77b10aa047d9bc60cf4301d1f5645f10f7b3
Mattéo Leullier <matteo.leullier@gmail.com>
2021-10-27 21:47:16 -0300

    fix spacing between theme switch and login buttons

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -2,7 +2,8 @@ import AppBar from '@mui/material/AppBar'
 import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
 import Button from '@mui/material/Button'
-import { IconButton } from '@mui/material'
+import IconButton from '@mui/material/IconButton'
+import { useTheme } from '@mui/material/styles'
 import MoonIcon from '@mui/icons-material/DarkMode'
 import SunIcon from '@mui/icons-material/LightMode'
 import useSharedTheme from '../hooks/useTheme'
@@ -10,7 +11,9 @@ import useSharedTheme from '../hooks/useTheme'
 interface NavProps { }
 
 const Nav: React.FC<NavProps> = (props) => {
-  const { theme, toggle } = useSharedTheme();
+  const { theme: paletteMode, toggle } = useSharedTheme();
+
+  const theme = useTheme();
 
   const switchThemes = () => toggle();
 
@@ -21,8 +24,8 @@ const Nav: React.FC<NavProps> = (props) => {
           Byteroom
         </Typography>
         <div style={{ flexGrow: 1 }} />
-        <IconButton onClick={switchThemes}>
-          {theme === 'dark' ? <SunIcon /> : <MoonIcon />}
+        <IconButton onClick={switchThemes} style={{ marginRight: theme.spacing(2) }}>
+          {paletteMode === 'dark' ? <SunIcon /> : <MoonIcon />}
         </IconButton>
         <Button color="info" variant="contained">
           Login
